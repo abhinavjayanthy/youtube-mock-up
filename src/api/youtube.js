d0762278296d036ebec71bd3e4059b5fd5c965a1
abhinavjayanthy <abhinavjayanthy@icloud.com>
2019-06-04 10:59:03 -0400

    working outube mock up

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,12 +1,10 @@
-import axios from "axiox";
-require("dotenv").config();
-const KEY = process.env.YOUTUBE_KEY;
+import axios from "axios";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
     part: "snippet",
     maxResults: 5,
-    key: KEY
+    key: "AIzaSyB8Zp5sOM_8m1Nf72us9IeHsXKoMeAveTM"
   }
 });
