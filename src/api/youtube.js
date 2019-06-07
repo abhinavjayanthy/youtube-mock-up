ea58140bedc5ddc04e7a8113f981aa50cb8cd6c9
abhinavjayanthy <abhinavjayanthy@icloud.com>
2019-06-07 12:05:16 -0400

    env for test

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-
+require("dotenv").config();
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
     part: "snippet",
     maxResults: 5,
-    key: "AIzaSyB8Zp5sOM_8m1Nf72us9IeHsXKoMeAveTM"
+    key: process.env.YOUTUBE_KEY
   }
 });
